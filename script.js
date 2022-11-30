3d407cd3605156275eabdebf9359b29a2b02e0ef
Leo Bruell <leobruell@gmail.com>
2022-11-30 10:27:51 -0500

    Make check winner function work mostly

--- script.js
+++ script.js
@@ -4,19 +4,23 @@ const gameBoard = (()=>{
         let aTile = document.createElement('div')
         aTile.classList.add('tile')
         aTile.setAttribute('tile-number',i + 1)
-        aTile.textContent = 'X'
+        aTile.addEventListener('click', clickTile)
         theBoard.push(aTile)
     }
     return {theBoard}
 })()
 
-const createPlayer = (name, letter) =>{
+const gameFlow = (()=>{
+    let turn = 'X'
+    return {turn}
+})()
+
+const createPlayer = () =>{
     const tiles = []
-    return {name, tiles, letter}
+    const name = ''
+    return {name, tiles}
 }
 
-let test = createPlayer('Leo')
-console.log(test.tiles)
 
 function showBoard(){
     let myBoard = document.querySelector('.board')
@@ -25,6 +29,61 @@ function showBoard(){
     }
 }
 
+function clickTile(event){
+    let theTile = event.target
+    const tileNumber = theTile.getAttribute('tile-number')
+    if (gameFlow.turn == 'X'){
+        theTile.textContent = 'X'
+        playerOne.tiles.push(tileNumber)
+        gameFlow.turn = 'O'
+        console.log(playerOne.tiles)
+    }
+    else if (gameFlow.turn == 'O'){
+        theTile.textContent = 'O'
+        playerTwo.tiles.push(tileNumber)
+        gameFlow.turn = 'X'
+        console.log(playerTwo.tiles)
+    }
+    theTile.removeEventListener('click', clickTile)
+    checkEnd()
+}
 
+function checkEnd(){
+    const endMessage = document.createElement('h2')
+    const body = document.querySelector('body')
+    const winningCombos = [['1','2','3'], ['4','5','6'], ['7','8','9'], ['1','4','7'], ['2','5','8'], ['3','6','9'],['1','5','9'],['3','5','7']]
+    function isSubset(combo, playerTiles){
+        return combo.every((element) => playerTiles.includes(element))
+    }
+    winningCombos.forEach(combo =>{
+        if (isSubset(combo, playerOne.tiles)){
+            endMessage.textContent = `${playerOne.name} wins!`
+            console.log(endMessage)
+            body.appendChild(endMessage)
+        }
+        else if (isSubset(combo, playerTwo.tiles)){
+            endMessage.textContent = `${playerTwo.name} wins!`
+            body.appendChild(endMessage) 
+        }
+    })
+    if (playerOne.tiles.length == 5){
+        endMessage.textContent = 'You tied!'
+        body.appendChild(endMessage)
+    }
+}
 
+function clickStart(event){
+    event.preventDefault()
+    const playerOneName = document.querySelector('.player-one input').value 
+    const playerTwoName = document.querySelector('.player-two input').value
+    playerOne.name = playerOneName
+    playerTwo.name = playerTwoName
+}
+
+//Starting Set-up:
+const playerOne = createPlayer()
+const playerTwo = createPlayer()
+const submitButton = document.querySelector('.submit-button')
+submitButton.addEventListener('click', clickStart)
 showBoard()
+
