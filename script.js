46b7433e4cdca3205da88e471ae4d8142c5f6ef7
Leo Bruell <leobruell@gmail.com>
2022-11-30 13:39:30 -0500

    Make js code work for 2 human players

--- script.js
+++ script.js
@@ -36,21 +36,26 @@ function clickTile(event){
         theTile.textContent = 'X'
         playerOne.tiles.push(tileNumber)
         gameFlow.turn = 'O'
-        console.log(playerOne.tiles)
     }
     else if (gameFlow.turn == 'O'){
         theTile.textContent = 'O'
         playerTwo.tiles.push(tileNumber)
         gameFlow.turn = 'X'
-        console.log(playerTwo.tiles)
     }
     theTile.removeEventListener('click', clickTile)
     checkEnd()
+
+    if (document.querySelector('.end-message').textContent.includes('win')){
+        let allTiles = document.querySelectorAll('.tile')
+        Array.from(allTiles).forEach(tile =>{
+            tile.removeEventListener('click', clickTile)
+        })
+    }
 }
 
 function checkEnd(){
-    const endMessage = document.createElement('h2')
     const body = document.querySelector('body')
+    const endMessage = document.querySelector('.end-message')
     const winningCombos = [['1','2','3'], ['4','5','6'], ['7','8','9'], ['1','4','7'], ['2','5','8'], ['3','6','9'],['1','5','9'],['3','5','7']]
     function isSubset(combo, playerTiles){
         return combo.every((element) => playerTiles.includes(element))
@@ -58,32 +63,60 @@ function checkEnd(){
     winningCombos.forEach(combo =>{
         if (isSubset(combo, playerOne.tiles)){
             endMessage.textContent = `${playerOne.name} wins!`
-            console.log(endMessage)
-            body.appendChild(endMessage)
         }
         else if (isSubset(combo, playerTwo.tiles)){
             endMessage.textContent = `${playerTwo.name} wins!`
-            body.appendChild(endMessage) 
         }
     })
-    if (playerOne.tiles.length == 5){
+    if (playerOne.tiles.length == 5 && document.querySelector('.end-message').textContent == null){
         endMessage.textContent = 'You tied!'
-        body.appendChild(endMessage)
     }
 }
 
-function clickStart(event){
+function clickNextOne(event){
     event.preventDefault()
-    const playerOneName = document.querySelector('.player-one input').value 
-    const playerTwoName = document.querySelector('.player-two input').value
-    playerOne.name = playerOneName
-    playerTwo.name = playerTwoName
+    if (document.querySelector('.player-one input').value != ''){
+        const playerOneName = document.querySelector('.player-one input').value 
+        playerOne.name = playerOneName
+        document.querySelector('.part-1').style.display = 'none'
+        document.querySelector('.part-2').style.display = 'grid'
+        document.querySelector('.next-2').addEventListener('click', clickNextTwo)
+    }
+    else{
+        const nameWarning = document.createElement('p')
+        nameWarning.classList.add('name-warning')
+        nameWarning.textContent = 'Please enter your name!'
+        const partOne = document.querySelector('.part-1')
+        partOne.appendChild(nameWarning)
+    }
 }
+
+function clickNextTwo(event){
+    event.preventDefault()
+    const opponentChoice = document.querySelector('input[name="question-2"]:checked').value
+    document.querySelector('.part-2').style.display = 'none'
+    if (opponentChoice == 'friend'){
+        document.querySelector('.part-3a').style.display = 'grid'
+        document.querySelector('.submit-button').addEventListener('click', clickStartA)
+    }
+    else{
+        document.querySelector('.part-3b').style.display = 'grid'
+    }
+}
+
+function clickStartA(event){
+    event.preventDefault()
+    const playerTwoName = document.querySelector('.player-two input').value 
+    playerTwo.name = playerTwoName
+    document.querySelector('.board').style.display = 'grid'
+    document.querySelector('.options').style.display = 'none'
+    showBoard()
+}
+
 
 //Starting Set-up:
 const playerOne = createPlayer()
 const playerTwo = createPlayer()
-const submitButton = document.querySelector('.submit-button')
-submitButton.addEventListener('click', clickStart)
-showBoard()
+const nextOneButton = document.querySelector('.next-1')
+nextOneButton.addEventListener('click', clickNextOne)
 
